bc8f199d57a67c98539cd4831680a45d499a718e
alihassan8989 <maqsoodia777@gmail.com>
2024-05-07 21:45:20 +0500

    Update

---
+++ app/components/video_com/Video_com.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Video_com = () => {
+  return <div>Video_com</div>;
+};
+
+export default Video_com;
---
+++ app/video/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Video_com from "../components/video_com/Video_com";
+
+const page = () => {
+  return (
+    <div>
+      <Video_com />
+    </div>
+  );
+};
+
+export default page;
